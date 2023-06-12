20cad50390559facd4e65255de61384570e61ad3
Yashraj <yashsharmau9@gmail.com>
2023-06-13 00:19:00 +0530

    did 5th question

---
+++ Questions/Q5/C.cpp
@@ -0,0 +1,16 @@
+/*
+Inorder Traversal of Binary Tree
+*/
+
+class Solution {
+public:
+    vector<int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL) {
+            inorderTraversal(root->left);
+            v.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return v;
+    }
+};
